2d2906e29e517b0f5fc5bd004f0ec37654fe7a94
rholderfield <raholderfield@gmail.com>
2021-10-25 19:05:57 -0700

    updates

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -1,5 +1,20 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.0;
+pragma solidity 0.6.6;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleCollectible is ERC721 {
+    uint256 public tokenCounter;
+
+    constructor() public ERC721("Dogie", "DOG") {
+        tokenCounter = 0;
+    }
+
+    function createCollectible() public returns (uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        tokenCounter++;
+        return newTokenId;
+    }
+}
