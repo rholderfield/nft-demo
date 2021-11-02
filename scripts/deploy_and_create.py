bd2e9455e05b6eaf68653877eb4620d4d8378b60
rholderfield <raholderfield@gmail.com>
2021-11-01 20:54:04 -0700

    updates

--- scripts/deploy_and_create.py
+++ scripts/deploy_and_create.py
@@ -1,8 +1,14 @@
 from scripts.helpful_scripts import get_account, SimpleCollectible
 
 sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
 
 
 def main():
     account = get_account()
     simple_collectible = SimpleCollectible.deploy({"from": account})
+    tx = simple_collectible.createCollectible(sample_token_uri, {"from": account})
+    tx.wait(1)
+    print(
+        f"You can view here {OPENSEA_URL.format(simple_collectible.address, simple_collectible.tokenCounter() - 1)}"
+    )
