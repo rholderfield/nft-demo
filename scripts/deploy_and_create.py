db3c33ab6f074273009edfb2f97ec5352cd8aabb
rholderfield <raholderfield@gmail.com>
2021-11-01 20:41:02 -0700

    update

--- scripts/deploy_and_create.py
+++ scripts/deploy_and_create.py
@@ -1,5 +1,8 @@
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import get_account, SimpleCollectible
+
+sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
 
 
 def main():
     account = get_account()
+    simple_collectible = SimpleCollectible.deploy({"from": account})
